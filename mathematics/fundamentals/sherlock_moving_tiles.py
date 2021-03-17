9d82e7af7e3fe197265764ac30e92a871b6dc0a0
exile79 <exile79@gmail.com>
2021-03-17 13:36:12 +0200

    Create sherlock_moving_tiles.py
    https://www.hackerrank.com/challenges/sherlock-and-moving-tiles/problem

---
+++ mathematics/fundamentals/sherlock_moving_tiles.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import os
+import sys
+import math
+
+#
+# Complete the movingTiles function below.
+#
+def movingTiles(l, s1, s2, queries):
+    results = [math.sqrt(2)*(l - math.sqrt(q))/abs(s2-s1) for q in queries]
+    return results
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    lS1S2 = input().split()
+
+    l = int(lS1S2[0])
+
+    s1 = int(lS1S2[1])
+
+    s2 = int(lS1S2[2])
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = int(input())
+        queries.append(queries_item)
+
+    result = movingTiles(l, s1, s2, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
